1ce0d57341619404a1c84871c07f9789dd460067
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-05-21 15:31:55 +0300

    moveLineLogic

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,7 +5,15 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
-class Game {
+export default class Game {
+  score = 0;
+  bestScore = 0;
+  init = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
   /**
    * Creates a new game instance.
    *
@@ -20,25 +28,38 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
+  constructor(state = this.init, gameStatus = 'idle') {
     // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.state = state;
+    this.gameStatus = gameStatus;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    return this.moveLineLogic(false, false);
+  }
+  moveRight() {
+    return this.moveLineLogic(false, true);
+  }
+  moveUp() {
+    return this.moveLineLogic(true, false);
+  }
+  moveDown() {
+    return this.moveLineLogic(true, true);
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.state;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +71,79 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.gameStatus;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.gameStatus = 'playing';
+    this.score = 0;
+
+    this.state = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 2, 2],
+    ];
+
+    return 'start game';
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.bestScore = this.score;
+    this.start();
+  }
 
   // Add your own methods here
-}
 
-module.exports = Game;
+  moveLine(line, isReversed) {
+    let reversed = isReversed ? [...line].reverse() : [...line];
+
+    reversed = reversed.filter((val) => val !== 0);
+
+    for (let j = 0; j < reversed.length - 1; j++) {
+      if (reversed[j] === reversed[j + 1]) {
+        reversed[j] *= 2;
+        this.score += reversed[j];
+        reversed.splice(j + 1, 1);
+      }
+    }
+
+    while (reversed.length < 4) {
+      reversed.push(0);
+    }
+
+    return isReversed ? reversed.reverse() : reversed;
+  }
+
+  //   Алгоритмічна стратегія:
+
+  // Транспонування матриці — це операція, яка віддзеркалює багатовимірний масив
+  // відносно його головної діагоналі.
+  // Перший рядок стає першим стовпцем, другий рядок
+  //  стає другим стовпцем і так далі.
+
+  transpose(state) {
+    return state[0].map((_, colIndex) => state.map((row) => row[colIndex]));
+  }
+
+  moveLineLogic(isVertical, isReversed) {
+    const saveState = this.state.toString();
+
+    const workingState = isVertical ? this.transpose(this.state) : this.state;
+
+    for (let i = 0; i < workingState.length; i++) {
+      workingState[i] = this.moveLine(workingState[i], isReversed);
+    }
+
+    this.state = isVertical ? this.transpose(workingState) : workingState;
+
+    return saveState !== this.state.toString();
+  }
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,78 @@
 'use strict';
+import Game from '../modules/Game.class';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+// Рандомити місце положення початкове цифорок
+// console.log(Game);
+const game = new Game();
 
 // Write your code here
+const gameField = document.querySelector('.game-field');
+const start = document.querySelector('.start');
+const startMessage = document.querySelector('.message-start');
+// const winMessage = document.querySelector('.message-win');
+// const loseMessage = document.querySelector('.message-lose');
+
+start.addEventListener('click', (e) => {
+  game.start();
+  startMessage.classList.add('hidden');
+
+  const state = game.getState();
+
+  renderRows(state);
+});
+// ArrowRight;
+// ArrowDown
+// ArrowDown;
+//  ArrowLeft
+
+function renderRows(state) {
+  state.forEach((rows, i) => {
+    rows.forEach((rowValue, j) => {
+      const cell = gameField.rows[i].cells[j];
+
+      if (rowValue !== 0) {
+        cell.textContent = rowValue;
+        cell.className = `field-cell field-cell--${rowValue}`;
+      } else {
+        cell.textContent = '';
+        cell.className = 'field-cell';
+      }
+    });
+  });
+}
+
+function handleInput(e) {
+  const gameStatus = game.getStatus();
+
+  if (gameStatus !== 'playing') {
+    return;
+  }
+
+  let isValidKey = true;
+
+  switch (e.code) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    default:
+      isValidKey = false;
+  }
+
+  const gameState = game.getState();
+
+  if (isValidKey) {
+    e.preventDefault();
+    renderRows(gameState);
+  }
+}
+
+document.addEventListener('keydown', handleInput);
